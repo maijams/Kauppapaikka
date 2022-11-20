269f237f9b631a21deec232b012699d179052f05
Maija <maija.saisa@helsinki.fi>
2022-11-20 19:26:32 +0200

    Fix routes.py

--- routes.py
+++ routes.py
@@ -1,9 +1,6 @@
 from app import app
-import visits
 from flask import render_template
 
 @app.route("/")
 def index():
-    visits.add_visit()
-    counter = visits.get_counter()
-    return render_template("index.html", counter=counter)
+    pass
